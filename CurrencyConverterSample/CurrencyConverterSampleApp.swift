66696c1ecfce28e963f416ebee9bb49749bf1b8b
Paulo Orquillo <olopsman@users.noreply.github.com>
2023-04-15 15:22:04 +1200

    Initial Commit

---
+++ CurrencyConverterSample/CurrencyConverterSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterSampleApp.swift
+//  CurrencyConverterSample
+//
+//  Created by Paulo Orquillo on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
